55d80aa111bf1d966dac2b9c96507810db5a5f86
Anbu34 <monarchfxlabs@gmail.com>
2025-09-04 19:55:22 +0000

    ec card genorator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-import "bootstrap";
-import "./style.css";
+function Generaterandomcard () {
+
+  const suit = ["spade", "clubs", "diamond", "heart"];
+  const value = [ "A","2","3","4","5","6","7","8","9","10","J","Q","K"];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const randomSuit = suit[Math.floor(Math.random() * suit.length)];
+  const randomValue = value[Math.floor(Math.random() * value.length )];
+  const suitsymbols ={
+    spade:"♠",clubs: "♣",diamond:"♦",heart:"♥"
+  };
+  return{
+    suit:randomSuit,
+    value:randomValue,
+    Symbol:suitsymbols[randomSuit],
+colorClass:
+      randomSuit === "hearts" || randomSuit === "Diamonds" ? "red" : "black"
+  };
+}
+function createcard (card){
+  const cardcontainer = document.getElementById("card-container");
+  cardcontainer.innerHTML = (`
+  <div class = "card">
+    <div class = "topleft ${card.colorClass} "> ${card.Symbol}
+    </div>
+    <div class = "center ${card.colorClass}"> ${card.value}  
+    </div>
+    <div class = "bottomright ${card.colorClass} "> ${card.Symbol} 
+    </div>
+  </div>
+  `);
+}
+const randomcard =  Generaterandomcard ();
+createcard (randomcard);
